d4eddb2b5e21be7e3172ba6a849db3f6e5eabce4
hailia sommerville <hailiassommerville@gmail.com>
2025-06-27 14:33:15 -0400

    task 2

--- database/user.js
+++ database/user.js
@@ -2,9 +2,13 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 // TASK 2: Define the User model here
-const User = db.define("user", {
-  // You should define the following columns:
+ // You should define the following columns:
   // - name: string, required
+const User = db.define("user", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
 });
 
 module.exports = User;
