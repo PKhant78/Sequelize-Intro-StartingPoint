1d0b57d0661b9a014c56853289166dac370ac34b
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-27 15:02:04 -0400

    Completed task 4

--- api/tasks.js
+++ api/tasks.js
@@ -11,45 +11,53 @@ router.get("/", async (req, res) => {
     const task = await Task.findAll();
     res.json(task);
   } catch (error) {
-    res.status(501).send("Not implemented");
+    res.status(501).send("Failed to get all tasks");
   }
   
 });
 
 // GET a single task by id
 router.get("/:id", async (req, res) => {
-  const task = Task.findByPk(req.params.id);
-  res.status.send(task)
-});
-
-router.get("/:id", async (req, res) => {
   try {
-    const task = Task.findByPk(req.params.id);
+    const task = await Task.findByPk(req.params.id);
     res.status.send(task)
   } catch (error) {
-    res.status(501).send("Not implemented");
-  }
-  
+    res.status(501).send("Failed to get task", req.params.id);
+  } 
 });
 
 // Patch a task by id
-router.patch("/:id", (req, res) => {
-  const task = Task.update(parseInt(req.params.id), req.body);
-  res.send(task);
+router.patch("/:id", async (req, res) => {
+  try {
+    const task = await Task.update(parseInt(req.params.id), req.body);
+    res.send(task);
+  } catch (error) {
+    res.status(501).send("Failed to patch task");
+  }
 });
 
 // Delete a task by id
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const task = Task.delete(id);
-  res.send(task);
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const task = await Task.delete(id);
+res.send(task);
+  }
+  catch (error) {
+    res.status(501).send("Failed to delete task");
+  }
 });
 
 
 // Create a new task
-router.post("/", (req, res) => {
-  const task = Task.create(req.body);
-  res.send(task);
+router.post("/", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.send(task);
+  }
+  catch (error) {
+    res.status(501).send("Failed to create task");
+  }
 });
 
 module.exports = router;
