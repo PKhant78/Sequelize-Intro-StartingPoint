acd994ca7c6c09f3c727c3e694e6909602a21621
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-27 14:27:30 -0400

    Finished task 1 in task.js

--- database/task.js
+++ database/task.js
@@ -2,11 +2,25 @@ const { DataTypes } = require("sequelize");
 const db = require("./db");
 
 // TASK 1: Define the Task model here
-const Task = db.define("task", {
   // You should define the following columns:
   // - title: string, required
   // - description: string, required
   // - completed: boolean, default false
+
+const Task = db.define("task", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  }
+
 });
 
 module.exports = Task;
